1c01ccdd7038c42742df5a6c666904142e31eb3b
Андрей <andrej.vinokurov@gmail.com>
2017-10-14 21:56:16 +0200

    pretty indent

--- IntermPerl/ExCh4-2.pl
+++ IntermPerl/ExCh4-2.pl
@@ -10,9 +10,9 @@ my @professor = qw(sunscreen water_bottle slide_rule batteries radio);
 my @gilligan  = qw(red_shirt hat lucky_socks water_bottle);
 
 my %all = (
-	Gilligan  => \@gilligan, 
-	Skipper   => \@skipper, 
-	Professor => \@professor,
+    Gilligan  => \@gilligan, 
+    Skipper   => \@skipper, 
+    Professor => \@professor,
 );
 
 check_items_for_all(\%all);
