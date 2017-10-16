1f861b6387c0d5fa2deaf979e33add21b1879fd0
Андрей <andrej.vinokurov@gmail.com>
2017-10-16 14:23:43 +0200

    chapter 4 exercise 3 done

--- IntermPerl/ExCh4-3.pl
+++ IntermPerl/ExCh4-3.pl
@@ -11,17 +11,36 @@ my %gilligan_info = (
 	hat      => 'White', 
 	shirt    => 'Red', 
 	position => 'First Mate',
+    location => 'The Island.',
 );
 my %skipper_info = (
 	name     => 'Skipper',
 	hat      => 'Black',
 	shirt    => 'Blue',
 	position => 'Captain',
+    location => 'The Island.',
 );
 
-my @crew = (\%gilligan_info, \%skipper_info);
+my @crew = (
+    \%gilligan_info,
+    \%skipper_info,
+    {
+        name => 'Mr. Howell',
+        hat => 'deep blue',
+        shirt => 'beige',
+        position=> 'passenger',
+        location => 'The Island Country Club.',
+    },
+    {
+        name => 'Mrs. Howell',
+        hat => 'pink',
+        shirt => 'violet',
+        position=> 'passenger',
+        location => 'The Island Country Club.',
+    },
+);
 
-my $format = "%-15s %-7s %-7s %-15s\n"; 
+my $format = "%-15s %-7s %-17s %-15s\n"; 
 printf $format, qw(Name Shirt Hat Position); 
 
 foreach my $crewmember (@crew) {
@@ -30,4 +49,10 @@ foreach my $crewmember (@crew) {
 		$crewmember->{'shirt'}, 
 		$crewmember->{'hat'}, 
 		$crewmember->{'position'};
+}
+
+## location
+say;
+foreach my $crewmember (@crew) {
+    say "$crewmember->{name} at $crewmember->{location}";
 }
