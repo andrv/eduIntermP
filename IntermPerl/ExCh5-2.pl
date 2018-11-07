1ed598012e72dba0ceb219b620f5cadba1ba44aa
Андрей <andrej.vinokurov@gmail.com>
2018-11-07 11:28:03 +0100

    exercise solved

--- IntermPerl/ExCh5-2.pl
+++ IntermPerl/ExCh5-2.pl
@@ -5,20 +5,6 @@ use warnings;
 use 5.014;
 use Data::Dumper;
 
-# Modify the code from this chapter so that each source machine’s
-# portion of the output shows the total bytes from that machine. List
-# the source machines in order from most to least data transferred.
-# Within each group, list the destination machines in order from most
-# to least data transferred to that target from the source machine:
-#
-#    professor.hut => gilligan.hut: 1845
-#    professor.hut => maryann.hut: 90
-#
-# The result should be that the machine that sent the most data will be
-# the first source machine in the list, and the first destination
-# should be the machine to which it sent the most data.
-#
-
 open(my $fh, "<", "ExCh5-2-coconet.dat")
     or die "Can't open < ExCh5-2-coconet.dat: $!";
 
@@ -30,13 +16,14 @@ while ( ! eof($fh) ) {
 
     my ($source, $destination, $bytes) = split;
     $total_bytes{$source}{$destination} += $bytes;
+    $total_bytes{$source}{summa} += $bytes;
 }
 close $fh;
 
 # print Dumper \%total_bytes;
 
-for my $source (sort keys %total_bytes) {
-    for my $destination (sort keys %{ $total_bytes{$source} }) {
+for my $source (sort { $total_bytes{$b}{summa} <=> $total_bytes{$a}{summa} } keys %total_bytes) {
+    for my $destination (sort { $total_bytes{$source}{$b} <=> $total_bytes{$source}{$a} } keys %{ $total_bytes{$source} }) {
         say "$source => $destination: $total_bytes{$source}{$destination} bytes";
     }
     say '';
