21b289b586bf88735d364e3af3a1fba4dc349a91
Андрей <andrej.vinokurov@gmail.com>
2018-11-07 12:45:57 +0100

    begin next exercise

---
+++ IntermPerl/ExCh5-3.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use 5.014;
+use Data::Dumper;
+
+open(my $fh, "<", "ExCh5-2-coconet.dat")
+    or die "Can't open < ExCh5-2-coconet.dat: $!";
+
+my %total_bytes;
+while ( ! eof($fh) ) {
+    defined( $_ = readline $fh )
+        or die "readline failed: $!";
+    # print $_;
+
+    my ($source, $destination, $bytes) = split;
+}
+close $fh;
+
+__END__
+Starting with your data structure from Exercise 2, rewrite the coconet.dat
+file so that it’s in the same format, but sorted by source machine. Report
+each destination machine once per source machine along with total bytes
+transferred. The destination machines should be indented under the source
+machine name and be sorted by the machine name:
+ginger.hut
+  maryann.hut 13744
+professor.hut
+  gilligan.hut 1845
+  maryann.hut 90
+thurston.howell.hut
+  lovey.howell.hut 97560
+...
