207e276161893fbec13c5f10213b05ada1933e71
Андрей <andrej.vinokurov@gmail.com>
2018-11-23 22:04:28 +0100

    ex 6 2 done

--- IntermPerl/ExCh6-2.pl
+++ IntermPerl/ExCh6-2.pl
@@ -5,7 +5,7 @@ use warnings;
 use 5.014;
 use Data::Dumper;
 
-use Storable qw(nstore);
+use JSON;
 
 open(my $fhr, "<", "ExCh5-2-coconet.dat")
     or die "Can't open < ExCh5-2-coconet.dat: $!";
@@ -18,11 +18,10 @@ while ( ! eof($fhr) ) {
 
     my ($source, $destination, $bytes) = split;
     $total_bytes{$source}{$destination} += $bytes;
-#    $total_bytes{$source}{summa} += $bytes;
 }
 close $fhr;
 
-#print Dumper \%total_bytes;
+#print to_json( \%total_bytes );
 
 #my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime(time - 15 * 86400);
 my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime(time);
@@ -31,7 +30,10 @@ $mon += 1; $year += 1900;
 #printf "%02d:%02d:%02d %d-%02d-%02d", $hour, $min, $sec, $year, $mon, $mday; say '';
 my $today = sprintf "%d-%02d-%02d", $year, $mon, $mday;
 
-nstore \%total_bytes, "totals-$today";
+open(my $fhw, '>', "ExCh6-2-out-$today")
+    or die "Can't open > ExCh6-2-out-$today: $!";
+print $fhw to_json( \%total_bytes );
+close $fhw;
 
 __END__
 Modify the programm from Exercise 1 to use JSON instead of Storable.
